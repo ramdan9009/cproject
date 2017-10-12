38e464f21047a97bc656f131fe8bdf83fdef8747
ramdan9009 <ramdan8285@students.amikom.ac.id>
2017-10-12 16:07:22 +0700

    versi 0.1

--- berwarna.c
+++ berwarna.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
-void textcolor(unsigned short color)
-{
-HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+void textcolor(unsigned short color){
+	HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
 
-SetConsoleTextAttribute(hCon,color);
+	SetConsoleTextAttribute(hCon,color);
 }
-void main()
-{
-clrscr();
-textcolor(1);printf(“teks ini menggunakan warna biru”);
-printf(“\n”);
-textcolor(2);printf(“teks ini menggunakan warna hijau”);
-printf(“\n”);
-textcolor(3);printf(“teks ini menggunakan warna cyan”);
-printf(“\n”);
-textcolor(4);printf(“teks ini menggunakan warna merah”);
-printf(“\n”);
-textcolor(5);printf(“teks ini menggunakan warna magenta”);
-printf(“\n”);
-textcolor(6);printf(“teks ini menggunakan warna brown”);
-printf(“\n”);
-textcolor(7);printf(“teks ini menggunakan warna lightgray”);
-printf(“\n”);
-textcolor(8);printf(“teks ini menggunakan warna darkgray”);
-printf(“\n”);
-getch();
+
+void main(){
+	clrscr();
+	textcolor(1);printf(“teks ini menggunakan warna biru”);
+	printf(“\n”);
+	textcolor(2);printf(“teks ini menggunakan warna hijau”);
+	printf(“\n”);
+	textcolor(3);printf(“teks ini menggunakan warna cyan”);
+	printf(“\n”);
+	textcolor(4);printf(“teks ini menggunakan warna merah”);
+	printf(“\n”);
+	textcolor(5);printf(“teks ini menggunakan warna magenta”);
+	printf(“\n”);
+	textcolor(6);printf(“teks ini menggunakan warna brown”);
+	printf(“\n”);
+	textcolor(7);printf(“teks ini menggunakan warna lightgray”);
+	printf(“\n”);
+	textcolor(8);printf(“teks ini menggunakan warna darkgray”);
+	printf(“\n”);
+	getch();
 }
